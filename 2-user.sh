e0457b443b261759f05fa85a0c11276bc3a1bb44
Juho Kontiainen <37773658+JuhoKon@users.noreply.github.com>
2021-11-19 11:14:16 +0200

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -69,19 +69,5 @@ konsave -i $HOME/ArchTitus/newprofile.knsv
 sleep 1
 konsave -a newprofile
 
-echo -e "Trying to do some other stuff."
-
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-fisher install jorgebucaran/nvm.fish
-
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-omf update && omf install batman
-
-nvm install latest
-npm install --global yarn
-
-## Fish terminaalissa tuo alempi
-fish && set --universal nvm_default_version latest
-
 echo -e "\nDone!\n"
 exit
